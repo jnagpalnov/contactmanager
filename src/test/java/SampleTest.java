1dead6b82ef6f817df7f71a1bba0378105320f98
Jatin <jnagpalnov@gmail.com>
2018-10-14 14:22:07 +0530

    sure fire plugin

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jatin on 10/14/2018.
+ */
+public class SampleTest {
+    @Test
+    public void hello(){
+        System.out.println("Sample Test");
+    }
+
+}
